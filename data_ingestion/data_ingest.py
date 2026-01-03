599049faa189a2b679e16cffbb69762af7546cf3
Sharathpd14 <sharathpd14@gmail.com>
2026-01-03 20:17:25 +0530

    data transformation is completed

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -4,11 +4,18 @@ import os
 import pandas as pd 
 from data_ingestion.data_transfer import DataConverter
 
+load_dotenv()
+
 
 class IngestData:
     def __init__(self):
-        pass
+        print("Ready")
+        self.product_data = pd.read_csv("data/flipkart_product_review.csv")
+        # print(self.product_data.head())
     
     def data_ingestion(self):
         pass
     
+    
+if __name__=="__main__":
+    IngestData()
--- data_ingestion/data_transfer.py
+++ data_ingestion/data_transfer.py
@@ -1,10 +1,42 @@
-import pandas
+import pandas as pd
 from langchain_core.documents import Document
 
 
 class DataConverter:
     def __init__(self):
-        pass
-    
+        self.product_data = pd.read_csv("data/flipkart_product_review.csv")
+        
+            
     def data_transformation(self):
-        pass
+        required_columns = list(self.product_data.columns[1:])
+        
+        product_list = []
+        
+        for index, row in self.product_data.iterrows():
+            obj = {
+                "product_name" : row["product_title"],
+                "product_rating": row["rating"],
+                "product_summary": row["summary"],
+                "product_review": row["review"]
+            }
+            
+            product_list.append(obj)
+        
+        docs =[]
+        for items in product_list:
+            metadata = {
+                "product_name":items["product_name"],
+                "product_rating":items["product_rating"],
+                "product_summary":items["product_summary"],
+            }
+            doc = Document(page_content=items["product_review"],metadata = metadata)
+            docs.append(doc)
+        
+        return docs
+    
+    
+    
+    
+if __name__=="__main__":
+    DataConverter().data_transformation()
+    
